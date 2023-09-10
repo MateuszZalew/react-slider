ca331fbe556b03fd798fa638a025e05e1b3f5bcc
zalesiu <zalewmat31@gmail.com>
2023-09-10 20:59:15 +0200

    add prev/next slide buttons and functionality

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,14 +1,40 @@
 import { shortList, list, longList } from "./data";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 const Slider = () => {
-  const [people, setPeople] = useState(shortList);
+  const [people, setPeople] = useState(longList);
+  const [currentPerson, setCurrentPerrson] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentPerrson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  const nextSlide = () => {
+    setCurrentPerrson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      console.log(result);
+      return result;
+    });
+  };
+
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, index) => {
         const { id, image, name, title, quote } = person;
         return (
-          <article className="slide" key={id}>
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (index - currentPerson)}%)`,
+              opacity: index === currentPerson ? 1 : 0,
+              visibility: index === currentPerson ? "visible" : "hidden",
+            }}
+            key={id}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
@@ -17,6 +43,12 @@ const Slider = () => {
           </article>
         );
       })}
+      <button type="button" className="prev" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <button type="button" className="next" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
     </section>
   );
 };
